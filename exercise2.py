73705b893d48571f1f5b617715c01aa4f981ba9e
Ben Wilson <ben.wilson@dkomplex.com>
2022-02-08 18:06:12 -0800

    Fix exercise 2

---
+++ exercise2.py
@@ -0,0 +1,8 @@
+"""Exercise 2 - Naming Rules"""
+a = 1
+b = 2
+c = 3
+d = 4
+
+def addVariables():
+    return(a + b + c + d)
---
+++ test_exercise2.py
@@ -0,0 +1,4 @@
+from exercise2 import addVariables
+
+def test_passing():
+    assert addVariables() == 10
