a0ef04228302e8c311f0dcb504b3802e93e4ea82
Ben Wilson <ben.wilson@dkomplex.com>
2022-02-08 17:44:18 -0800

    Add exercise 5

---
+++ exercise5.py
@@ -0,0 +1,3 @@
+"""Exercise 5 - List Comprehension"""
+def returnSquares():
+    return [x for x in range(10)]
---
+++ test_exercise5.py
@@ -0,0 +1,4 @@
+from exercise5 import returnSquares
+
+def test_passing():
+    assert returnSquares() == [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
