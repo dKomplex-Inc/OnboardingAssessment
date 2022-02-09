7dfa06192181d8da97582f9223f5082a8b95bcd9
Ben Wilson <ben.wilson@dkomplex.com>
2022-02-08 17:38:18 -0800

    Add exercise 1

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+"""Exercise 1 - Variable Updating"""
+a = 2
+a = 4
+a = 6
+
+def returnValue():
+    value = (a + a + a)
+    return(value)
---
+++ test_exercise1.py
@@ -0,0 +1,4 @@
+from exercise1 import returnValue
+
+def test_passing():
+    assert returnValue() == 12
